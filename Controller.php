c4fdf68388cb5b74a00ad70e6ffae00d4a359e18
Honassis Lopes <honassislopes@gmail.com>
2020-01-26 21:03:26 -0300

    melhorias css, renomeacao de arquivos de .php para .html

--- Controller.php
+++ Controller.php
@@ -9,11 +9,29 @@ $r1 = isset($_POST['r1']) ? $_POST['r1'] : "";
 $r2 = isset($_POST['r2']) ? $_POST['r2'] : "";
 switch ($_POST['action']) {
     case 'search':
-        print_r(Main::init($r1, $r2));
+        ?>
+      <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/styles.css">
+    <title>Result</title>
+</head>
+<body class="body"> 
+    <div class="center">
+    <span id="txt">
+    <?=Main::init($r1, $r2)[0]['text']?>
+    </span>
+</div>
+</body> 
+</html>
+        <?php
     break;
     case 'create':
         Main::create($r1);
-        header('location:index.php');
+        header('location:index.html');
     break;
 }
 
