d6d417f2dc1d909acecb0f75e95ab0db191f6fca
Honassis Lopes <honassislopes@gmail.com>
2020-01-29 09:03:12 -0300

    pesquisa por nome

--- Controller.php
+++ Controller.php
@@ -6,7 +6,16 @@ include_once "classes/database.class.php";
 include_once "classes/main.class.php";
 
 $r1 = isset($_POST['r1']) ? $_POST['r1'] : "";
+if(strlen($r1)<=9){
+    $r1 = "NO_SEARCH_NO_SEARCH";
+}
 $r2 = isset($_POST['r2']) ? $_POST['r2'] : "";
+$r2 = intval($r2);
+if($r2>200000&&$r2<300000){
+    //tranquilo
+}else{
+$r2="NO_SEARCH_NO_SEARCH";
+}
 switch ($_POST['action']) {
     case 'search':
         ?>
@@ -22,7 +31,15 @@ switch ($_POST['action']) {
 <body class="body"> 
     <div class="center">
     <span id="txt">
-    <?=Main::init($r1, $r2)[0]['text']?>
+    <?php
+    @$msg = Main::init($r1, $r2)[0]['text'];
+    if($msg==null){
+        echo "<h1>Erro Na Pesquisa</h1> <a href='index.html'>Voltar</a>";
+    }else{
+        echo $msg;
+    }
+    
+    ?>
     </span>
 </div>
 </body> 
